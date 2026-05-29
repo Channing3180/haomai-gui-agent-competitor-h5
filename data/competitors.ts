4a1370c88e315bda432014980791e13e2f36e32a
Channing3180 <chaoran3180@gmail.com>
2026-05-29 09:01:34 -0700

    chore: 周报自动更新 2026-05-29

--- data/competitors.ts
+++ data/competitors.ts
@@ -42,7 +42,7 @@ export type Competitor = {
   lastChecked: string;
 };
 
-export const reportDate = "2026-05-28";
+export const reportDate = "2026-05-29";
 
 export const competitors: Competitor[] = [
   {
